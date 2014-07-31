5c19e863f423186ef1fe51beaaa19bb279902cc8
Sean Stephenson <sms@pixar.com>
2014-07-31 10:58:19 -0700

    print time each second

--- stopwatch/stopwatch.py
+++ stopwatch/stopwatch.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python
 
+# http://www.reddit.com/r/dailyprogrammer/comments/pjsdx/difficult_challenge_2/
+
+# Your mission is to create a stopwatch program.
+
+# This program should have start, stop, and lap options,
+# and it should write out to a file to be viewed later.
+
+import time
+
+def procedure():
+    time.sleep(1.0)
+
 def stopwatch():
-    print 'stopwatch'
+    start = time.clock()
+    for i in range(1,10):
+      print i
+      procedure()
+
 
 def main():
     stopwatch()
