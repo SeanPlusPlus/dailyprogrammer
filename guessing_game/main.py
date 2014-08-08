f260d181114cc1b37857d5943aaee1712b0a6cc2
Sean Stephenson <sms@pixar.com>
2014-08-08 16:43:12 -0700

    add recursive and iterative solutions

--- guessing_game/main.py
+++ guessing_game/main.py
@@ -5,15 +5,15 @@ from random import randint
 def bisect(start, end):
    return int((start + end) / 2)
 
-def calc(lower, mid, upper):
-    question = 'Is the num [h]igher, [l]ower, or [e]xactly ' + str(mid) + ': '
+def calcRecur(lower, upper):
+    question = 'Is the num [h]igher, [l]ower, or [e]xactly ' + str(bisect(lower, upper)) + ': '
     direction = str(raw_input(question))
 
     if direction == 'h':
-      calc(mid, bisect(mid, upper), upper)
+      calcRecur(bisect(lower, upper), upper)
 
     elif direction == 'l':
-      calc(lower, bisect(lower, mid) ,mid)
+      calcRecur(lower, bisect(lower, upper))
 
     elif direction == 'e':
       print 'got it!'
@@ -21,13 +21,35 @@ def calc(lower, mid, upper):
     else:
       print 'no dice'
 
+def calcIter(lower, upper):
+    mid = bisect(upper, lower)
+
+    while(True):
+        question = 'Is the num [h]igher, [l]ower, or [e]xactly ' + str(mid) + ': '
+        direction = str(raw_input(question))
+
+        if direction == 'h':
+            lower = mid
+            mid = bisect(mid, upper)
+
+        elif direction == 'l':
+            upper = mid
+            mid = bisect(lower, mid)
+
+        elif direction == 'e':
+            print 'got it!'
+            break
+
+        else:
+            print 'no dice'
 
 def main():
     lower = 1
     upper = 100
     randNumber = randint(lower, upper)
     print 'The Random Number Is: ' + str(randNumber) + '\n'
-    calc(lower, 50, upper)
+    calcRecur(lower, upper)
+    calcIter(lower, upper)
 
 
 if __name__ == '__main__':
