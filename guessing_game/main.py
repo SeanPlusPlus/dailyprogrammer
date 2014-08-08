c2d4768dbcfab6a178280d8198ac775c2c07ef80
Sean Stephenson <sms@pixar.com>
2014-08-08 16:21:21 -0700

    solve recursively

--- guessing_game/main.py
+++ guessing_game/main.py
@@ -1,7 +1,34 @@
 #!/usr/bin/env python
 
+from random import randint
+
+def bisect(start, end):
+   return int((start + end) / 2)
+
+def calc(lower, mid, upper):
+    question = 'Is the num [h]igher, [l]ower, or [e]xactly ' + str(mid) + ': '
+    direction = str(raw_input(question))
+
+    if direction == 'h':
+      calc(mid, bisect(mid, upper), upper)
+
+    elif direction == 'l':
+      calc(lower, bisect(lower, mid) ,mid)
+
+    elif direction == 'e':
+      print 'got it!'
+
+    else:
+      print 'no dice'
+
+
 def main():
-    print 'main'
+    lower = 1
+    upper = 100
+    randNumber = randint(lower, upper)
+    print 'The Random Number Is: ' + str(randNumber) + '\n'
+    calc(lower, 50, upper)
+
 
 if __name__ == '__main__':
     main()
